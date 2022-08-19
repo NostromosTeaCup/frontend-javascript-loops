add74c085c7d309a830f57b5256558bda154d0a9
NostromosTeaCup <lvanmierlo@protonmail.com>
2022-08-19 11:57:59 +0200

    loops oefenopdracht

--- loops-and-arrays.js
+++ loops-and-arrays.js
@@ -1,6 +1,7 @@
 // ==========================================
 // Opdracht 1
-// Schrijf een script dat iedere naam in onderstaande array vervangt door een koosnaampje, door er -"je" achter te plakken.
+// Schrijf een script dat iedere naam in onderstaande array vervangt door een koosnaampje, door er -"je"
+// achter te plakken.
 // Zorg ervoor dat dit ook zou werken als de array wel 100 of 200 namen zou bevatten!
 // ==========================================
 
@@ -11,7 +12,6 @@ const names = ["Henk", "Piet", "Fred", "Joop"];
 // console.log(names) geeft: ["Henk", "Piet", "Fred", "Joop"]
 // Na jouw script zie je de aangepaste waardes:
 // console.log(names) geeft: ["Henkje", "Pietje", "Fredje", "Joopje"]
-
 
 
 
@@ -28,7 +28,6 @@ const numbers = [2, 4, 5, 29, 38];
 // console.log(numbers) geeft: [2, 4, 5, 29, 38];
 // Na jouw script zie je de aangepaste waardes:
 // console.log(numbers) geeft: [ 4, 8, 15, 87, 76 ];
-
 
 
 
@@ -53,3 +52,4 @@ const squares = [30, 2, 8, 24, 11];
 //   'Het volume van 24 is 13824',
 //   'Het volume van 11 is 1331'
 // ]
+
--- loops.js
+++ loops.js
@@ -8,6 +8,7 @@
 
 
 
+
 // ==========================================
 // Opdracht 2. Maak een for-loop die 4 keer het woord "loop..." logt, en bij de vijfde loop het woord "klaar!"
 // Verwachte uitkomsten:
@@ -17,7 +18,6 @@
 // loop...
 // klaar!
  // ==========================================
-
 
 
 // ==========================================
@@ -32,7 +32,6 @@
 
 // Tip: een getal is oneven als je het door 2 deelt en er blijft nog iets over. Een getal is even als je het deelt door 2 en er blijft niets over. Google daarom eens naar 'mdn webdocs remainder operator'.
 // ==========================================
-
 
 
 // ==========================================
